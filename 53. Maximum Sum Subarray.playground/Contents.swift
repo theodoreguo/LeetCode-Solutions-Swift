49fe01111cbdcda148029d9a760592f598cb5c30
theodoreguo <jsguozitong@gmail.com>
2017-02-16 22:55:01 +0800

    Modified comments

--- 53. Maximum Sum Subarray.playground/Contents.swift
+++ 53. Maximum Sum Subarray.playground/Contents.swift
@@ -14,7 +14,7 @@ import Foundation
  Let's denote that:
  f(k) = Maximum sum of subarray ending at index k.
  Then, f(k) = max(f(k-1) + nums[k], nums[k]).
- Using an array of size n, We could deduce the final answer by as f(n – 1), with the initial state of f(0) = A[0]. Since we only need to access its previous element at each step, two variables are sufficient.
+ Using an array of size n, we could deduce the final answer by as f(n – 1), with the initial state of f(0) = A[0]. Since we only need to access its previous element at each step, two variables are sufficient.
  Notice the difference between the two: maxEndingHere and maxSoFar; the former is the maximum sum of subarray that must end at index k, while the latter is the global maximum subarray sum.
  */
 class Solution {
