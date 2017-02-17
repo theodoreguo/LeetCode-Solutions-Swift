a7b01674c2840550bd596f89181f9a607f0827bf
theodoreguo <jsguozitong@gmail.com>
2017-02-17 15:16:10 +0800

    Modified question's sequence no.

--- 28. Implement strStr().playground/Contents.swift
+++ 28. Implement strStr().playground/Contents.swift
@@ -1,6 +1,6 @@
 /**
  Question:
- Implement strsStr().
+ Implement strStr().
  Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
  
  Link: https://oj.leetcode.com/problems/implement-strstr/
