fb2869e9cee3d958166a38cd6ad1c2b1d7336fb6
theodoreguo <jsguozitong@gmail.com>
2017-03-03 14:58:39 +0800

    Added 61. Rotate List

--- 61. Rotate List.playground/Contents.swift
+++ 61. Rotate List.playground/Contents.swift
@@ -9,3 +9,59 @@
  Link: https://leetcode.com/problems/rotate-list/
  */
 
+/**
+ Definition for singly-linked list
+ */
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    /**
+     Implementation using Runner Tech: use two pointers, i.e., slow and fast
+     */
+    func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+        if head == nil { return head }
+        
+        var slow = head
+        var fast = head
+        let len = getLength(head)
+        var k = k % len
+        
+        // Move fast in front so that the gap between slow and fast becomes k
+        while k > 0 {
+            fast = fast!.next
+            k -= 1
+        }
+        
+        // Move both slow and fast concurrently until fast reaches the end, maintaining the gap
+        while fast!.next != nil {
+            slow = slow!.next
+            fast = fast!.next
+        }
+        
+        // Join the unshifted list with the head
+        fast!.next = head
+        fast = slow!.next
+        slow!.next = nil
+        
+        return fast
+    }
+    
+    private func getLength(_ head: ListNode?) -> Int {
+        var len = 0
+        var node = head
+        
+        while node != nil {
+            len += 1
+            node = node!.next
+        }
+        
+        return len
+    }
+}
